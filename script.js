00c4d91acbb02ed11746379b05e3727de684b286
deliags <deliagsmith@outlook.com>
2022-02-09 14:47:01 +0000

    first iteration of playRound function

--- script.js
+++ script.js
@@ -6,5 +6,25 @@ const computerChoice = ["Rock", "Paper", "Scissors"];
 //This will be called every time it is the computer's turn
 function computerPlay() {
   const randomComputerChoice = Math.floor(Math.random() * computerChoice.length);
-  console.log(randomComputerChoice, computerChoice[randomComputerChoice]);
+  return computerChoice[randomComputerChoice];
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = prompt("Hi! This is your round! Choose one: Rock, Paper or Scissors", "Your Choice").toLowerCase();
+  computerSelection = computerPlay().toLowerCase(); //Need to lowecase
+  let results;
+
+  if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock") {
+    
+    results = "You Win!!";
+  }
+  
+  else if (playerSelection == computerSelection) {
+    results = "A tie!";
+  }
+  
+  else {
+    results = "You Loose!";
+  }
+  return results;
 }
