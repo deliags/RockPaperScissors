03999288ebd56c1ef05e456942585a85b2742ee8
deliags <deliagsmith@outlook.com>
2022-02-09 18:59:28 +0000

    Refractored game logic

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-
-//The list of choices available for the computer to pick
-const computerChoice = ["Rock", "Paper", "Scissors"];
+//Player score and Computer Score are initialised
+let playerScore = 0;
+let computerScore = 0;
 
 //When called it returns a random item from the array "computerChoice"
 //This will be called every time it is the computer's turn
 function computerPlay() {
+  //The list of choices available for the computer to pick
+  const computerChoice = ["Rock", "Paper", "Scissors"];
   const randomComputerChoice = Math.floor(Math.random() * computerChoice.length);
   return computerChoice[randomComputerChoice];
 }
 
-let playerScore = 0;
-let computerScore = 0;
-  
-function playRound(playerSelection, computerSelection) {
-  playerSelection = prompt("Hi! This is your round! Choose one: Rock, Paper or Scissors", "Your Choice").toLowerCase();
-  
-  computerSelection = computerPlay().toLowerCase();
+//The Player is asked to choose a move between Rock, Paper and Scissors
+let playerSelection = prompt("Hi! This is your round! Choose one: Rock, Paper or Scissors", "Your Choice").toLowerCase();
 
-  if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock") {
+//The computer chooses randomly
+let computerSelection = computerPlay().toLowerCase();
+
+function playRound(playerSelection, computerSelection) { 
+
+  if (
+    playerSelection == "rock" && computerSelection == "scissors" ||
+    playerSelection == "scissors" && computerSelection == "paper" ||
+    playerSelection == "paper" && computerSelection == "rock") {
+    
+    //Increase Player score as they have won the round
     playerScore++;
   }
   
   else if (playerSelection == computerSelection) {
+    
+    //Both Player and Computer get a score as they selected the same option
     playerScore++;
     computerScore++;
   }
   
   else {
+
+    //Computer has won this round
     computerScore++;
   }
-  return `${playerScore}, ${computerScore}`;
+
+  return `Your score is: ${playerScore}, the Computer score is: ${computerScore}`;
 }
 
 function game() {
