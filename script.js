0a2a9f3106bb21e11b3d8c6f19508ac5eef07bb2
deliags <deliagsmith@outlook.com>
2022-02-09 21:58:05 +0000

    Working commit

--- script.js
+++ script.js
@@ -2,40 +2,35 @@
 let playerScore = 0;
 let computerScore = 0;
 
+//The list of choices available for the computer to pick
+const computerChoice = ["Rock", "Paper", "Scissors"];
+
 //When called it returns a random item from the array "computerChoice"
 //This will be called every time it is the computer's turn
 function computerPlay() {
-  //The list of choices available for the computer to pick
-  const computerChoice = ["Rock", "Paper", "Scissors"];
+  
+  //Calculates random choice for computer
   const randomComputerChoice = Math.floor(Math.random() * computerChoice.length);
   return computerChoice[randomComputerChoice];
 }
 
-//The Player is asked to choose a move between Rock, Paper and Scissors
-let playerSelection = prompt("Hi! This is your round! Choose one: Rock, Paper or Scissors", "Your Choice").toLowerCase();
-
-//The computer chooses randomly
-let computerSelection = computerPlay().toLowerCase();
-
-function playRound(playerSelection, computerSelection) { 
-
-  if (
-    playerSelection == "rock" && computerSelection == "scissors" ||
-    playerSelection == "scissors" && computerSelection == "paper" ||
-    playerSelection == "paper" && computerSelection == "rock") {
+function playRound(playerSelection, computerSelection) {
+  
+  //All combinations when the player wins
+  if ((playerSelection == "rock" && computerSelection == "scissors") ||
+      (playerSelection == "scissors" && computerSelection == "paper") ||
+      (playerSelection == "paper" && computerSelection == "rock")){
     
     //Increase Player score as they have won the round
     playerScore++;
-  }
-  
-  else if (playerSelection == computerSelection) {
-    
+
+  } else if (playerSelection == computerSelection) {
+
     //Both Player and Computer get a score as they selected the same option
     playerScore++;
     computerScore++;
-  }
-  
-  else {
+
+  } else {
 
     //Computer has won this round
     computerScore++;
@@ -45,21 +40,25 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  let result;
-
+  
+  //Five rounds
   for (let i = 0; i < 5; i++) {
-    playRound();
+    //The Player is asked to choose a move between Rock, Paper and Scissors
+    let playerSelection = prompt("Hi! This is your round! Choose one: Rock, Paper or Scissors", "Your Choice").toLowerCase();
+
+    //The computer chooses randomly
+    let computerSelection = computerPlay().toLowerCase();
+
+    console.log(playRound(playerSelection, computerSelection));
   }
-  if (playerScore == 5 && playerScore > computerScore) {
-    result = "You Win!";
-  }
-  else if(computerScore == 5 && playerScore < computerScore){
-    result = "You Loose!"
-  }
-  else {
-    result = "A Tie!"
-  }
-  return result;
+
+  //How the score is calculated
+  if (playerScore > computerScore) {
+    console.log(`You're the winner! Final Score: ${playerScore} to ${computerScore}`);
+  } else if (computerScore > playerScore) {
+    console.log(`You're the loser. Final Score: ${playerScore} to ${computerScore}`);
+  } else
+    console.log(`You tied. Final Score: ${playerScore} to ${computerScore}`);
 }
 
 game();
