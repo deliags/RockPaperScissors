565cec69705584cbe084442d3b6dc8b7ba293919
deliags <deliagsmith@outlook.com>
2022-02-17 15:36:19 +0000

    displays each round information and the end result

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ const computerChoice = ["Rock", "Paper", "Scissors"];
 //When called it returns a random item from the array "computerChoice"
 //This will be called every time it is the computer's turn
 function computerPlay() {
-  
+
   //Calculates random choice for computer
   const randomComputerChoice = Math.floor(Math.random() * computerChoice.length);
   return computerChoice[randomComputerChoice];
 }
 
 function playRound(playerSelection, computerSelection) {
-  
+
   //All combinations when the player wins
   if ((playerSelection == "rock" && computerSelection == "scissors") ||
-      (playerSelection == "scissors" && computerSelection == "paper") ||
-      (playerSelection == "paper" && computerSelection == "rock")){
-    
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")) {
+
     playerScore++;
 
   } else if (playerSelection == computerSelection) {
@@ -37,38 +37,36 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  
+
   let turn = 0;
+  const buttons = document.querySelectorAll('button');
+  const resultDiv = document.getElementById("result");
+  const turnResultH2 = document.createElement("h2");
 
   //Five rounds
   for (let i = 0; i < 5; i++) {
-
-    //The Player is asked to choose a move between Rock, Paper and Scissors
-    let playerSelection = prompt(
-      "Hi! This is your round! Choose one: Rock, Paper or Scissors", "Your Choice").toLowerCase();
   
-    //The computer chooses randomly
-    let computerSelection = computerPlay().toLowerCase();
-
+    //returns the element button upon clicking on it
+    const choice = buttons.forEach(button => button.addEventListener('click', (e) => {
+      playRound(button.id, computerPlay().toLowerCase());}));
+    
     turn++;
 
-    alert(`End of Round Number: ${turn}.` + playRound(playerSelection, computerSelection));
+    turnResultH2.textContent = `End of Round Number: ${turn}.` + choice;
+    resultDiv.append(turnResultH2);
   }
 
+  const endResult = document.createElement("p");
+  
   //How the score is calculated
   if (playerScore > computerScore) {
-    alert(`You're the winner! Final Score: ${playerScore} to ${computerScore}`);
+    endResult.textContent = `You're the winner! Final Score: ${playerScore} to ${computerScore}`;
   } else if (computerScore > playerScore) {
-    alert(`You're the loser. Final Score: ${playerScore} to ${computerScore}`);
+    endResult.textContent = `You're the loser. Final Score: ${playerScore} to ${computerScore}`;
   } else
-    alert(`You tied. Final Score: ${playerScore} to ${computerScore}`);
+    endResult.textContent = `You tied. Final Score: ${playerScore} to ${computerScore}`;
+  
+  resultDiv.append(endResult);
 }
 
-// game();
-
-const buttons = document.querySelectorAll('button');
-
-//returns the element button upon clicking on it
-buttons.forEach(button => button.addEventListener('click', (e) => {
-  console.log(button)
-}))
+game();
